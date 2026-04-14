f13a12cd8db4624863648e59efe864167d36595d
somosa <joma828@gmail.com>
2026-04-14 19:12:04 +0600

    filter add

--- src/Component/Timeline/Timeline.jsx
+++ src/Component/Timeline/Timeline.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { TimelineContext } from '../Context/TimelineContext';
 import callImg from "../../assets/call.png";
 import textImg from "../../assets/text.png";
@@ -10,94 +10,101 @@ import { Link } from 'react-router';
 
 
 const Timeline = () => {
-      const { timeline } = useContext(TimelineContext);
-      console.log(timeline)
-    
-    return (
-      <div className="max-w-6xl mx-auto mt-10 mb-10 px-4">
-        <h1 className="text-4xl font-bold mb-6 text-slate-800">Timeline</h1>
-        <label className="select mb-10">
-          <span className="label">Filter timeline</span>
-          <select>
-            <option>ALL</option>
-            <option>Call</option>
-            <option>Video</option>
-            <option>Text</option>
-          </select>
-        </label>
+  const { timeline } = useContext(TimelineContext);
+  console.log(timeline);
 
-        {timeline.length === 0 ? (
-          <div className="flex flex-col items-center justify-center text-center bg-white shadow rounded-xl p-10">
-            <FiClock size={60} className="text-gray-400 mb-4" />
+  const [filter, setFilter] = useState("all");
 
-            <h2 className="text-2xl font-bold text-slate-700">
-              No Activity Yet
-            </h2>
+  const filteredTimeline =
+    filter === "all" ? timeline: timeline.filter((item) => item.actionType === filter);
 
-            <p className="text-gray-500 mt-2">
-              You havent made any call, text or video yet.
-            </p>
+  return (
+    <div className="max-w-6xl mx-auto mt-10 mb-10 px-4">
+      <h1 className="text-4xl font-bold mb-6 text-slate-800">Timeline</h1>
+      <label className="select mb-10">
+        <span className="label">Filter timeline</span>
+        <select onChange={(e) => setFilter(e.target.value)}>
+          <option value="all">ALL</option>
+          <option value="call">Call</option>
+          <option value="video">Video</option>
+          <option value="text">Text</option>
+        </select>
+      </label>
 
-            <Link to="/">
-              <button className="mt-5 flex items-center gap-2 bg-black text-white px-5 py-2 rounded-lg hover:bg-gray-800">
-                <FiHome />
-                Go Home
-              </button>
-            </Link>
-          </div>
-        ) : (
-          <div className="space-y-4">
-            {timeline.map((item, index) => (
-              <div
-                key={index}
-                className="bg-white shadow rounded-xl p-5 flex justify-between items-center"
-              >
-                <div className="flex items-center gap-4">
-                  <img
-                    src={
-                      item.actionType === "call"
-                        ? callImg
-                        : item.actionType === "text"
-                          ? textImg
-                          : videoImg
-                    }
-                    alt={item.actionType}
-                    className="w-12 h-12 object-cover"
-                  />
+      {timeline.length === 0 ? (
+        <div className="flex flex-col items-center justify-center text-center bg-white shadow rounded-xl p-10">
+          <FiClock size={60} className="text-gray-400 mb-4" />
 
-                  <div>
-                    <h2 className="font-bold text-lg text-slate-800">
-                      {item.actionType} With <span className='text-[15px] text-gray-500'> {item.name}</span>
-                    </h2>
-                    <p className="text-sm text-gray-500">{item.email}</p>
-                  </div>
-                </div>
+          <h2 className="text-2xl font-bold text-slate-700">No Activity Yet</h2>
 
-                <div className="flex items-center gap-2">
-                  {item.actionType === "call" && (
-                    <span className="flex items-center gap-2 bg-green-100 text-green-700 px-3 py-1 rounded-full text-sm font-semibold">
-                      <FiPhone /> {item.next_due_date}
+          <p className="text-gray-500 mt-2">
+            You havent made any call, text or video yet.
+          </p>
+
+          <Link to="/">
+            <button className="mt-5 flex items-center gap-2 bg-black text-white px-5 py-2 rounded-lg hover:bg-gray-800">
+              <FiHome />
+              Go Home
+            </button>
+          </Link>
+        </div>
+      ) : (
+        <div className="space-y-4">
+          {filteredTimeline.map((item, index) => (
+            <div
+              key={index}
+              className="bg-white shadow rounded-xl p-5 flex justify-between items-center"
+            >
+              <div className="flex items-center gap-4">
+                <img
+                  src={
+                    item.actionType === "call"
+                      ? callImg
+                      : item.actionType === "text"
+                        ? textImg
+                        : videoImg
+                  }
+                  alt={item.actionType}
+                  className="w-12 h-12 object-cover"
+                />
+
+                <div>
+                  <h2 className="font-bold text-lg text-slate-800">
+                    {item.actionType} With{" "}
+                    <span className="text-[15px] text-gray-500">
+                      {" "}
+                      {item.name}
                     </span>
-                  )}
-
-                  {item.actionType === "text" && (
-                    <span className="flex items-center gap-2 bg-blue-100 text-blue-700 px-3 py-1 rounded-full text-sm font-semibold">
-                      <FiMessageSquare /> {item.next_due_date}
-                    </span>
-                  )}
-
-                  {item.actionType === "video" && (
-                    <span className="flex items-center gap-2 bg-purple-100 text-purple-700 px-3 py-1 rounded-full text-sm font-semibold">
-                      <FiVideo /> {item.next_due_date}
-                    </span>
-                  )}
+                  </h2>
+                  <p className="text-sm text-gray-500">{item.email}</p>
                 </div>
               </div>
-            ))}
-          </div>
-        )}
-      </div>
-    );
-};
+
+              <div className="flex items-center gap-2">
+                {item.actionType === "call" && (
+                  <span className="flex items-center gap-2 bg-green-100 text-green-700 px-3 py-1 rounded-full text-sm font-semibold">
+                    <FiPhone /> {item.next_due_date}
+                  </span>
+                )}
+
+                {item.actionType === "text" && (
+                  <span className="flex items-center gap-2 bg-blue-100 text-blue-700 px-3 py-1 rounded-full text-sm font-semibold">
+                    <FiMessageSquare /> {item.next_due_date}
+                  </span>
+                )}
+
+                {item.actionType === "video" && (
+                  <span className="flex items-center gap-2 bg-purple-100 text-purple-700 px-3 py-1 rounded-full text-sm font-semibold">
+                    <FiVideo /> {item.next_due_date}
+                  </span>
+                )}
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};;
 
 export default Timeline;
